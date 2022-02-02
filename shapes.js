d3499ce918ea1af6105231b5225169f33586ad08
andarroyave <andarroyave@gmail.com>
2022-02-01 22:14:17 -0500

    Se agregan estilos al form

--- shapes.js
+++ shapes.js
@@ -14,12 +14,25 @@ function squareArea(side){
 
 //Triangle
 
+function isoscelesTriangleHeight(side1, side2, base){
+    console.log(side1 + '-'+ side2 + '-' + base)
+    if(side1 != side2 && base != side1 && base != side2){
+        alert("El triangulo no es isosceles");
+        console.log("El triangulo no es isosceles")
+        return 0;
+    }
+    console.log(side1 + '-'+ side2 + '-' + base)
+    let height = Math.sqrt(side1**2 - (base/2)**2);
+    return height;
+}
+
 function trianglePerimeter(side1, side2, base){
     return side1 + side2 + base;
 }
 
-function triangleArea(height, base){
-    return (base*height)/2;
+function triangleArea(side1, side2, base){
+    console.log(side1 + '-'+ side2 + '-' + base)
+    return (base*isoscelesTriangleHeight(side1, side2, base))/2;
 }
 
 //Circle
@@ -35,8 +48,10 @@ function circlePerimeter(radius){
 
 //Interaccion con HTML
 
+//Square
+
 function calculateSquarePerimeter(){
-    const input = document.getElementById("SquareInput");
+    const input = document.getElementById("square_input");
     const value = input.value;
     const perimeter = squarePerimeter(value);
     alert(perimeter)
@@ -44,8 +59,28 @@ function calculateSquarePerimeter(){
 }
 
 function calculateSquareArea(){
-    const input = document.getElementById("SquareInput");
+    const input = document.getElementById("square_input");
     const value = input.value;
     const area = squareArea(value);
+    alert(area)
+}
+
+//Triangle
+
+function calculateTrianglePerimeter(){
+    const side1 = Number(document.getElementById("TriangleSideA").value);
+    const side2 = Number(document.getElementById("TriangleSideB").value);
+    const base = Number(document.getElementById("TriangleBase").value);
+    const perimeter = trianglePerimeter(side1, side2, base);
+    alert(perimeter)
+
+}
+
+function calculateTriangleArea(){
+    const side1 = Number(document.getElementById("TriangleSideA").value);
+    const side2 = Number(document.getElementById("TriangleSideB").value);
+    const base = Number(document.getElementById("TriangleBase").value);
+    console.log(side1 + '-'+ side2 + '-' + base)
+    const area = triangleArea(side1, side2, base);
     alert(area)
 }
